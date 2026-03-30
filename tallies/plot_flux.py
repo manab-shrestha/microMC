c924c2a15cd737c276b0d36432dd9c9b8bf40927
manab shrestha <shrestha.manab@pm.me>
2026-03-30 02:38:20 +0100

    steel ball fun

--- tallies/plot_flux.py
+++ tallies/plot_flux.py
@@ -1,12 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
-from cycler import cycler
 import serpentTools
 import sys
 
-og_matlab_colors = [(0, 0, 1),(0, 1, 0),(1, 0, 0),(0, 1, 1),(1, 0, 1),(1, 1, 0),(0, 0, 0)]
-plt.rcParams.update({"axes.prop_cycle": cycler(color=og_matlab_colors),"lines.linewidth": 1.5,"lines.linestyle": "-","axes.grid": False,"font.size": 12,})
+plt.rcParams.update({"axes.prop_cycle": plt.cycler(color=plt.cm.Set1.colors),"lines.linewidth": 1.5,"lines.linestyle": "-","axes.grid": False,"font.size": 12,})
 
 # --- Config ---
 n_particles = 1000
@@ -52,20 +50,19 @@ boundary_indices = np.arange(n_boundaries)
 boundary_labels = np.arange(n_boundaries - 1, -1, -1)  # 69, 68, ..., 1, 0
 
 # --- Plot ---
-fig, ax = plt.subplots(figsize=(12, 6))
+fig, ax = plt.subplots(figsize=(7, 4.5))
 
-ax.step(boundary_indices[:-1], phi_micromc_plot, where="post", ls="--", label="microMC")
-ax.step(boundary_indices[:-1], phi_serpent_plot, where="post", label="Serpent")
+ax.step(boundary_indices[:-1], phi_micromc_plot, where="post", label="microMC")
+ax.step(boundary_indices[:-1], phi_serpent_plot, where="post", ls="--", label="Serpent")
 
-ax.set_xlabel("WIMS69 Energy Group Boundary")
-ax.set_ylabel(r"Flux per energy group, $\phi$")
-ax.set_title("Neutron Flux Spectrum — microMC vs Serpent")
+ax.set_xlabel("WIMS172 Energy Groups")
+ax.set_ylabel(r"$\phi$")
 ax.set_xlim(0, n_boundaries - 1)
 
-ax.set_xticks([])
+#ax.set_xticks([])
 
 ax.legend()
 
 plt.tight_layout()
-plt.savefig("flux_comparison.png", dpi=150)
+plt.savefig("flux_spectra.png", dpi=400)
 plt.show()
